2e0ac00bbe5331b52925ecb9a8888805efc75446
Abdullah Ahmed <abdullahahmedpk18@gmail.com>
2024-09-30 23:37:07 -0500

    Bismillah

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ExampleSubsystem.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+
+public class ExampleSubsystem extends SubsystemBase {
+    /**
+     * Creates a new ExampleSubsystem.
+     */
+    public ExampleSubsystem() {
+
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
